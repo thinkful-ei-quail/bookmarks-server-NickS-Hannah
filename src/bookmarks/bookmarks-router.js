3b30eeaf6f010beea2da8935ddc14a1bec1e62d1
Nick Snyder <N_Snyder119@hotmail.com>
2020-08-14 11:16:22 -0700

    added logger and store

---
+++ src/bookmarks/bookmarks-router.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const express = require('express');
+const {v4: uuid} = require('uuid');
+const logger = require('logger');
+const {bookmarks} = require('../store'); 
+const bookmarksRouter = express.Router();
+const bodyParser = express.json();
+
+
+
+bookmarksRouter
+  .route('/bookmarks')
+  .get((req,res) => {
+    res
+      .json(bookmarks);
+  })
+  .post(bodyParser,(req,res)=> {
+    const {title, content} = req.body;
+    if(!title) {
+      logger.error('Title is required.');
+      return res
+        .status(404)
+        .send('Title is required.');
+    }
+    if(!content) {
+      logger.error(`Content with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Content is required.');
+    }
+    const id = uuid();
+    const newBookmark = {
+      id,
+      title,
+      content
+    };
+    bookmarks.push(newBookmark);
+    res.status(201)
+      .location(`http://localhost:8000/bookmarks/${id}`)
+      .json(newBookmark);
+    //form validation goes here   
+  });
+bookmarksRouter
+  .route('/bookmarks/:id')
+  .get((req,res) => {
+    const {id} = req.params;
+    // eslint-disable-next-line eqeqeq
+    const bookmark = bookmarks.find(b => b.id == id);
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Bookmark not found');
+    }
+    res.json(bookmark);
+  })
+  .delete((req,res) => {
+    //form validation goes here
+  });
+module.exports = bookmarksRouter;
---
+++ src/store.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const bookmarks =[{
+  id:1,
+  title:'bookmark',
+  content:'content bookmark 1'
+}];
+
+module.exports = bookmarks;
